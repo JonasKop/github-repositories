178199b17cef82b28c51270c914da3943c715829
Jonas Sjödin <jonas.k.sjodin@gmail.com>
2024-01-03 21:59:08 +0100

    Adding terraform configuration for all repositories (#2)

---
+++ terraform/terraform.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {}
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.42.0"
+    }
+  }
+}
+
+provider "github" {}
+
+variable "repositories" {
+  default = []
+}
+
+resource "github_repository" "repos" {
+  for_each    = var.repositories
+  name        = each.key
+  description = each.value.description
+  visibility  = "public"
+
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_squash_merge          = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+
+  has_issues = true
+}
+
+resource "github_branch_protection" "main_branch" {
+  for_each      = var.repositories
+  repository_id = github_repository.repos[each.key].node_id
+  pattern       = "main"
+}
